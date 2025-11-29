2c912c6175d914b3341667c2588d6f36aaf5b1a0
unknown <ahmedphysical90@gmail.com>
2025-11-29 18:41:50 +0800

    Some basic_operations on python

---
+++ basic_operations.py
@@ -0,0 +1,7 @@
+result = 10 + 5  
+# Addition: result will be 15
+difference = 14 - 7  
+# Subtraction: difference will be 7
+product = 3 * 4  
+# Multiplication: product will be 12
+print("result")
